eaf1a61cf5a8f08721679521fd0119a95efe8332
M7700 <moataz7sha@gmail.com>
2024-12-31 18:21:45 +0300

    flutter_application_2

---
+++ main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+void mo(){
+print("object");
+
+}
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+
+
+    ),
+    home: Scaffold(
+      backgroundColor: Colors.amber,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+
+        actions: [IconButton(onPressed: mo, icon: Icon(Icons.share)),
+        
+        
+        ],
+        
+        
+
+
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.black87,
+        
+
+      ),
+      
+      
+    ),
+    
+
+
+
+  ));
+}
